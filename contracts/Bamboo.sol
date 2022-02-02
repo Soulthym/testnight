10e1687351b0ba942443a99063d6f4fabde78fac
robichi <rob@ichi.org>
2022-02-01 20:42:06 -0700

    only reduce transaction fees

--- contracts/Bamboo.sol
+++ contracts/Bamboo.sol
@@ -12,6 +12,7 @@ contract Bamboo is ERC20Burnable, Ownable {
   string private constant SYMBOL = "BAM";
   string private constant NAME = "Bamboo";
   uint256 private constant INITIAL_SUPPLY = 100000000 * 10**9; // 100 million, precision 9
+  uint256 private constant INITIAL_FEE = 20; 
   uint256 private constant PRECISION = 10**9;
   uint256 private constant PERCENT = 100;
   address private constant NULL_ADDRESS = address(0);
@@ -31,6 +32,8 @@ contract Bamboo is ERC20Burnable, Ownable {
   {
     require(_pool != NULL_ADDRESS, "Bamboo::constructor: null pool address");
     _mint(_msgSender(), INITIAL_SUPPLY);
+    setBuyFeePercent(INITIAL_FEE);
+    setSellFeePercent(INITIAL_FEE);
     emit Deployed(_msgSender(), _pool, SYMBOL, NAME);
   }
 
@@ -38,12 +41,14 @@ contract Bamboo is ERC20Burnable, Ownable {
     return 9;
   }
 
-  function setBuyFeePercent(uint256 feePercent) external onlyOwner {
+  function setBuyFeePercent(uint256 feePercent) public onlyOwner {
+    require(feePercent < feeBuy, "Bamboo::setBuyFeePercent: can only reduce transaction fees");
     feeBuy = feePercent;
     emit SetBuyFee(_msgSender(), feePercent);
   }
 
-  function setSellFeePercent(uint256 feePercent) external onlyOwner {
+  function setSellFeePercent(uint256 feePercent) public onlyOwner {
+    require(feePercent < feeSell, "Bamboo::setSellFeePercent: can only reduce transaction fees");
     feeSell = feePercent;
     emit SetSellFee(_msgSender(), feePercent);
   }
